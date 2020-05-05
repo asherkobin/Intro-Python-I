9ba64b5b3e3266f303e43f2beb7ce7c93aa779d4
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-05-04 21:03:09 -0400

    First Batch

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,29 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+
+for prop in sys.argv:
+  print("argv: " + prop)
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+
+print("platform: " + sys.platform)
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
 
+print("Python Version: " + f"{((sys.version_info.major))}.{((sys.version_info.minor))}.{((sys.version_info.micro))}")
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+
+print("Current PID: " + str(os.getpid()))
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+
+print("CWD: " + os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+
+print("Machine's Login Name: " + os.getlogin())
